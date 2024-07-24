b0145b493b434c4bbed223cb29f4b80cd7c80d9f
cmadoka2003 <148872395+cmadoka2003@users.noreply.github.com>
2024-07-24 22:50:13 +0200

    HTML + CSS + JS (Desktop) de la page Classement Fini

--- assets/js/app.js
+++ assets/js/app.js
@@ -112,11 +112,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
-            /* Toggle between adding and removing the "active" class,
-            to highlight the button that controls the panel */
             this.classList.toggle("active");
 
-            /* Toggle between hiding and showing the active panel */
             var panel = this.nextElementSibling;
             if (panel.style.display === "block") {
                 panel.style.display = "none";
